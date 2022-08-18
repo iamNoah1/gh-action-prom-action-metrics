df36dedba72e3cbc9f382266b8f9ebabaec13e9a
iamnoah <noahispas@gmail.com>
2022-08-18 10:35:53 +0200

    first shot

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/iamNoah1/prometheus-action-metrics
+
+go 1.19
+
+require (
+	github.com/castai/promwrite v0.4.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/prometheus/prometheus v2.5.0+incompatible // indirect
+	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
+	golang.org/x/sys v0.0.0-20220817070843-5a390386f1f2 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220817144833-d7fd3f11b9b1 // indirect
+	google.golang.org/grpc v1.48.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	promremote "github.com/castai/promwrite"
+)
+
+var (
+	prometheusRemoteHost     string
+	prometheusRemoteUsername string
+	prometheusRemotePassword string
+)
+
+func main() {
+	if prometheusRemoteHost == "" || prometheusRemotePassword == "" || prometheusRemoteUsername == "" {
+		log.Fatal("Invalid options")
+	}
+
+	ctx := context.Background()
+	cli := promremote.NewClient(prometheusRemoteHost)
+	headers := make(map[string]string)
+
+	userPass := prometheusRemoteUsername + ":" + prometheusRemotePassword
+	encodedPass := base64.StdEncoding.EncodeToString([]byte(userPass))
+	headers["Authorization"] = fmt.Sprintf("Basic %s", encodedPass)
+
+	authorizationHeader := promremote.WriteHeaders(headers)
+
+	metric := promremote.WriteRequest{
+		TimeSeries: []promremote.TimeSeries{
+			{
+				Labels: []promremote.Label{
+					{
+						Name:  "__name__",
+						Value: "ghaction_metric",
+					},
+				},
+				Sample: promremote.Sample{
+					Time:  time.Now(),
+					Value: rand.Float64() * 100,
+				},
+			},
+		},
+	}
+
+	_, err := cli.Write(ctx, &metric, authorizationHeader)
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+
+}
